b4e198a6b5231aba920181829ea561171c0a3e63
Kenta Sato <bicycle1885@gmail.com>
2017-08-02 15:15:47 +0900

    deploy docs using Julia 0.6

--- docs/make.jl
+++ docs/make.jl
@@ -4,5 +4,5 @@ makedocs()
 deploydocs(
     deps=Deps.pip("mkdocs", "pygments", "mkdocs-material"),
     repo="github.com/BioJulia/BioAlignments.jl.git",
-    julia="0.5",
+    julia="0.6",
     osname="linux")
