b9fa15acebbd75f8cd4e20b4281819fca2d9f8b4
Ben J. Ward <ward9250@gmail.com>
2018-06-02 11:47:08 +0100

    Switch to native Documenter.jl html generation

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,23 @@
 using Documenter, BioAlignments
 
 makedocs(
-    modules=[BioAlignments])
+    format = :html,
+    sitename = "BioAlignments.jl",
+    pages = [
+        "Home" => "index.md",
+        "Alignment representation" => "alignments.md",
+        "Pairwise alignment" => "pairalign.md",
+        "High-throughput sequencing" => "hts-files.md",
+        "References" => "references.md",
+        "Contributing" => "contributing.md"
+    ],
+    authors = "Kenta Sato, Ben J. Ward, The BioJulia Organisation and other contributors."
+)
 deploydocs(
-    deps=Deps.pip("mkdocs", "pygments", "mkdocs-material"),
-    repo="github.com/BioJulia/BioAlignments.jl.git",
-    julia="0.6",
-    osname="linux")
+    repo = "github.com/BioJulia/BioAlignments.jl.git",
+    julia = "0.6",
+    osname = "linux",
+    target = "build",
+    deps = nothing,
+    make = nothing
+)
