54aaab02c98f71cd99c2320d3ca1e6ca221e9b75
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-07-26 16:55:08 -0500

    Clarify the docstring for alignment(::PairwiseAlignmentResult)
    alignment(::AlignedSequence) and alignment(::PairwiseAlignmentResult)
    both return an Alignment, but alignment(::PairwiseAlignmentResult) has
    always returned a PairwiseAlignment. I don't want to break the current
    implementation for alignment(::PairwiseAlignmentResult), but the the
    implementation of the two others makes sense in the context of their types.
    Ideally, I would separate the alignment(::PairwiseAlignmentResult) function
    into separate getter functions like
    alignment(::PairwiseAlignmentResult)::Alignment and
    pairwise_alignment(::PairwiseAlignmentResult)::PairwiseAlignment, but
    again, that would be breaking. So instead, settle for a really
    dirty-feeling compromise of documenting the difference and explaining the
    reasoning behind it with the hope that BioAlignments v4 is able to
    reconcile this bandaged solution.

--- src/pairwise/result.jl
+++ src/pairwise/result.jl
@@ -53,7 +53,10 @@ hasalignment(aln::PairwiseAlignmentResult) = aln.aln !== nothing
 """
     alignment(alignment_result)
 
-Return alignment if any.
+Return the alignment if any as a [`PairwiseAlignment`](@ref). To get the
+[`Alignment`](@ref), nest the function, e.g. `alignment(alignment(alignment_result))`.
+This function returns a `PairwiseAlignment` instead of an `Alignment` for
+backwards-compatibility reasons.
 
 See also: `hasalignment`
 """
