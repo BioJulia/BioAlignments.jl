99d6c0dd3c2c02a982c59578f4d1d0a6a3590d28
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2022-07-26 16:23:04 -0500

    Add sequence getter function for PairwiseAlignmentResult

--- src/pairwise/result.jl
+++ src/pairwise/result.jl
@@ -64,6 +64,13 @@ function alignment(aln::PairwiseAlignmentResult)
     return aln.aln
 end
 
+"""
+    sequence(alignment_result)
+
+Return the query sequence of `alignment_result`, if it exists.
+"""
+sequence(aln::PairwiseAlignmentResult) = sequence(alignment(aln))
+
 
 # Printer
 # -------
